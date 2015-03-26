842c2108df7a4719c5c340be815ccb911a7ebd84
kluo8 <kluo8@users.noreply.github.com>
2015-03-26 00:01:04 -0400

    Update factfile.pl

--- factfile.pl
+++ factfile.pl
@@ -1,3 +1,12 @@
 % start(state).
 % transition(current-state, condition, next-state).
 % end(state).
+
+% Top-level states
+state(dormant).
+state(init).
+state(idle).
+state(monitoring).
+state(error_diagnosis).
+
+% States under init state
